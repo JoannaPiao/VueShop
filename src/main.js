dca766b5767e891df7a2e88c61a21346895e95d5
xuyichen <757502262@qq.com>
2019-02-26 10:41:36 +0800

    '外卖界面完成'

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import app from './app'
+import router from './router'
+
+import Header from './components/Header/Header'
+
+//注册全局组件
+Vue.component('Header',Header)
+
+new Vue({
+  el: '#app',
+  render: h => h(app),
+  router,
+})
